54149f078f8692dcc310691c666b7ad39d784d6c
DenimCity <altidorj1@gmail.com>
2019-11-06 10:27:04 -0500

    connect to databse

--- server.js
+++ server.js
@@ -4,6 +4,18 @@ import Sequelize from 'sequelize';
 const app = express();
 const PORT = 8001;
 
-app.listen(PORT, () => {
-	console.log(`Running server on port ${PORT}`);
+const connection = new Sequelize('db', 'user', 'password', {
+	host: 'local',
+	dialect: 'sqlite',
+	storage: 'db.sqlite'
 });
+
+connection
+	.authenticate()
+	.then(() => {
+		console.log('Connection to Database created');
+		app.listen(PORT, () => {
+			console.log(`Running server on port ${PORT}`);
+		});
+	})
+	.catch(() => console.log('Issue connectin to database'));
